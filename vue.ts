5be7e821e4b75eb977e4790a50842628442af57c
Johnson Chu <johnsoncodehk@gmail.com>
2024-09-25 21:52:28 +0800

    feat: add vue compatibility

---
+++ vue.ts
@@ -0,0 +1,35 @@
+import {
+	signal as _signal,
+	computed as _computed,
+	currentEffectScope,
+} from './index.js';
+
+export {
+	effect,
+	effectScope,
+} from './index.js';
+
+export function shallowRef<T>(value: T) {
+	const s = _signal(value);
+	return {
+		get value() {
+			return s();
+		},
+		set value(value: T) {
+			s.set(value);
+		}
+	};
+}
+
+export function computed<T>(fn: () => T) {
+	const c = _computed(fn);
+	return {
+		get value() {
+			return c();
+		}
+	};
+}
+
+export function getCurrentEffectScope() {
+	return currentEffectScope;
+}
