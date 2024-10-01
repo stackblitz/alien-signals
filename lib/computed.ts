64a7ac9a201211740b4382eeb9b314d111cdc00c
Johnson Chu <johnsoncodehk@gmail.com>
2024-10-02 03:57:16 +0800

    feat: add equalityComputed API

--- lib/computed.ts
+++ lib/computed.ts
@@ -20,10 +20,7 @@ export class Computed<T = any> implements Dependency, Subscriber {
 	update() {
 		if (Subscriber.isDirty(this)) {
 			const lastActiveSub = Subscriber.startTrack(this);
-			if (!Object.is(
-				this.oldValue,
-				this.oldValue = this.getter(this.oldValue)
-			)) {
+			if (this.oldValue !== (this.oldValue = this.getter(this.oldValue))) {
 				Subscriber.endTrack(this, lastActiveSub);
 				Dependency.propagate(this);
 			}
@@ -37,10 +34,7 @@ export class Computed<T = any> implements Dependency, Subscriber {
 		Dependency.link(this);
 		if (Subscriber.isDirty(this)) {
 			const lastActiveSub = Subscriber.startTrack(this);
-			if (!Object.is(
-				this.oldValue,
-				this.oldValue = this.getter(this.oldValue)
-			)) {
+			if (this.oldValue !== (this.oldValue = this.getter(this.oldValue))) {
 				Subscriber.endTrack(this, lastActiveSub);
 				Dependency.propagate(this);
 			}
@@ -52,6 +46,68 @@ export class Computed<T = any> implements Dependency, Subscriber {
 	}
 }
 
+export class EqualityComputed<T = any> extends Computed<T> {
+	constructor(
+		getter: () => T,
+	) {
+		super(oldValue => {
+			const newValue = getter();
+			if (this.equals(oldValue, newValue)) {
+				return oldValue!;
+			}
+			return newValue;
+		});
+	}
+
+	equals(a: any, b: any): boolean {
+		if (a === b) {
+			return true;
+		}
+
+		if (a === null || b === null || typeof a !== typeof b) {
+			return false;
+		}
+
+		if (typeof a === 'object') {
+			if (Array.isArray(a) && Array.isArray(b)) {
+				if (a.length !== b.length) {
+					return false;
+				}
+				for (let i = 0; i < a.length; i++) {
+					if (!this.equals(a[i], b[i])) {
+						return false;
+					}
+				}
+				return true;
+			}
+
+			if (!Array.isArray(a) && !Array.isArray(b)) {
+				for (const key in a) {
+					if (a.hasOwnProperty(key)) {
+						if (!b.hasOwnProperty(key) || !this.equals(a[key], b[key])) {
+							return false;
+						}
+					}
+				}
+				for (const key in b) {
+					if (b.hasOwnProperty(key) && !a.hasOwnProperty(key)) {
+						return false;
+					}
+				}
+				return true;
+			}
+
+			return false; // One is array and the other is not
+		}
+
+		return false;
+	}
+}
+
 export function computed<T>(getter: (oldValue?: T) => T) {
 	return new Computed(getter);
+}
+
+export function equalityComputed<T>(getter: () => T) {
+	return new EqualityComputed(getter);
 }
