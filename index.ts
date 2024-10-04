86e2a50d49af7baddac0fbf641a50fda0c84566f
Johnson Chu <johnsoncodehk@gmail.com>
2024-10-04 20:47:47 +0800

    refactor: split effectScope file

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from './lib/computed';
 export * from './lib/effect';
+export * from './lib/effectScope';
 export * from './lib/signal';
 export * from './lib/system';
---
+++ lib/effectScope.ts
@@ -0,0 +1,51 @@
+import { Effect } from './effect';
+
+export class EffectScope {
+	effects: Effect | undefined = undefined;
+	effectsTail: Effect | undefined = undefined;
+	onDispose: (() => void)[] = [];
+
+	run<T>(fn: () => T) {
+		const original = currentEffectScope;
+		try {
+			currentEffectScope = this;
+			return fn();
+		} finally {
+			currentEffectScope = original;
+		}
+	}
+
+	stop() {
+		while (this.effects !== undefined) {
+			this.effects.stop();
+		}
+		for (const cb of this.onDispose) {
+			cb();
+		}
+	}
+
+	add(effect: Effect) {
+		if (this.effectsTail !== undefined) {
+			this.effectsTail.nextEffect = effect;
+			effect.prevEffect = this.effectsTail;
+			this.effectsTail = effect;
+		} else {
+			this.effects = this.effectsTail = effect;
+		}
+	}
+
+	remove(effect: Effect) {
+		if (effect.prevEffect !== undefined) {
+			effect.prevEffect.nextEffect = effect.nextEffect;
+		} else {
+			this.effects = effect.nextEffect;
+		}
+		if (effect.nextEffect !== undefined) {
+			effect.nextEffect.prevEffect = effect.prevEffect;
+		} else {
+			this.effectsTail = effect.prevEffect;
+		}
+	}
+}
+
+export let currentEffectScope = new EffectScope();
