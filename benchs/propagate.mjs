f132beeb704b21e33705d84f28875be14f6d37d0
Johnson Chu <johnsoncodehk@gmail.com>
2024-12-22 10:16:17 +0800

    refactor(bench): move complex benchmark from complex.mjs to propagate.mjs

--- benchs/propagate.mjs
+++ benchs/propagate.mjs
@@ -18,6 +18,24 @@ boxplot(() => {
 	})
 		.args('h', [1, 10, 100])
 		.args('w', [1, 10, 100]);
+
+	bench('complex: $w * $h', function* (state) {
+		const w = state.get('w');
+		const h = state.get('h');
+		const src = signal({ w, h });
+		for (let i = 0; i < w; i++) {
+			let last = src;
+			for (let j = 0; j < h; j++) {
+				const prev = last;
+				last = computed(() => ({ [`${i}-${j}`]: prev.get() }));
+			}
+			effect(() => last.get());
+		}
+
+		yield () => src.set({ upstream: src.get() });
+	})
+		.args('h', [1, 10, 100])
+		.args('w', [1, 10, 100]);
 });
 
 run({ format: 'markdown' });
