18c7051141d58a2f93bee45bcf4083764bf2c399
Johnson Chu <johnsoncodehk@gmail.com>
2024-12-23 13:27:09 +0800

    refactor: enhance condition check in shallowPropagate for SubscriberFlags

--- src/system.ts
+++ src/system.ts
@@ -228,7 +228,7 @@ export function shallowPropagate(link: Link): void {
 	do {
 		const updateSub = link.sub;
 		const updateSubFlags = updateSub.flags;
-		if (updateSubFlags & SubscriberFlags.ToCheckDirty) {
+		if ((updateSubFlags & (SubscriberFlags.ToCheckDirty | SubscriberFlags.Dirty)) === SubscriberFlags.ToCheckDirty) {
 			updateSub.flags = updateSubFlags | SubscriberFlags.Dirty;
 		}
 		link = link.nextSub!;
