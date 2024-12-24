8d54167ff0fd81f1ef88d0e18ff293ffb1864ec4
Johnson Chu <johnsoncodehk@gmail.com>
2024-12-25 05:22:53 +0800

    feat: add asyncComputed, asyncEffect (#24)

---
+++ src/unstable/asyncComputed.ts
@@ -0,0 +1,66 @@
+import { Computed } from '../computed.js';
+import { nextTrackId } from '../effect.js';
+import { Dependency, endTrack, link, shallowPropagate, startTrack, SubscriberFlags } from '../system.js';
+import { asyncCheckDirty } from './asyncSystem.js';
+
+export function asyncComputed<T>(getter: (cachedValue?: T) => AsyncGenerator<Dependency, T>): AsyncComputed<T> {
+	return new AsyncComputed<T>(getter);
+}
+
+export class AsyncComputed<T = any> extends Computed {
+
+	async get(): Promise<T> {
+		const flags = this.flags;
+		if (flags & SubscriberFlags.Dirty) {
+			if (await this.update()) {
+				const subs = this.subs;
+				if (subs !== undefined) {
+					shallowPropagate(subs);
+				}
+			}
+		} else if (flags & SubscriberFlags.ToCheckDirty) {
+			if (await asyncCheckDirty(this.deps!)) {
+				if (await this.update()) {
+					const subs = this.subs;
+					if (subs !== undefined) {
+						shallowPropagate(subs);
+					}
+				}
+			} else {
+				this.flags = flags & ~SubscriberFlags.ToCheckDirty;
+			}
+		}
+		return this.currentValue!;
+	}
+
+	// @ts-expect-error
+	async update(): Promise<boolean> {
+		try {
+			startTrack(this);
+			const trackId = nextTrackId();
+			const oldValue = this.currentValue;
+			const generator = this.getter(oldValue);
+			let current = await generator.next();
+			while (!current.done) {
+				const dep = current.value;
+				if (dep.lastTrackedId !== trackId) {
+					dep.lastTrackedId = trackId;
+					link(dep, this);
+				}
+				current = await generator.next();
+
+				// if (this.flags & SubscriberFlags.Recursed) {
+				// 	return await this.get() !== oldValue;
+				// }
+			}
+			const newValue = await current.value;
+			if (oldValue !== newValue) {
+				this.currentValue = newValue;
+				return true;
+			}
+			return false;
+		} finally {
+			endTrack(this);
+		}
+	}
+}
---
+++ src/unstable/asyncSystem.ts
@@ -0,0 +1,72 @@
+import { IComputed, Link, shallowPropagate, SubscriberFlags } from "../system";
+
+export async function asyncCheckDirty(link: Link): Promise<boolean> {
+	let stack = 0;
+	let dirty: boolean;
+	let nextDep: Link | undefined;
+
+	top: do {
+		dirty = false;
+		const dep = link.dep;
+		if ('update' in dep) {
+			const depFlags = dep.flags;
+			if (depFlags & SubscriberFlags.Dirty) {
+				if (await dep.update()) {
+					const subs = dep.subs!;
+					if (subs.nextSub !== undefined) {
+						shallowPropagate(subs);
+					}
+					dirty = true;
+				}
+			} else if (depFlags & SubscriberFlags.ToCheckDirty) {
+				const depSubs = dep.subs!;
+				if (depSubs.nextSub !== undefined) {
+					depSubs.prevSub = link;
+				}
+				link = dep.deps!;
+				++stack;
+				continue;
+			}
+		}
+		if (dirty || (nextDep = link.nextDep) === undefined) {
+			if (stack) {
+				let sub = link.sub as IComputed;
+				do {
+					--stack;
+					const subSubs = sub.subs!;
+					let prevLink = subSubs.prevSub!;
+					if (prevLink !== undefined) {
+						subSubs.prevSub = undefined;
+						if (dirty) {
+							if (await sub.update()) {
+								shallowPropagate(sub.subs!);
+								sub = prevLink.sub as IComputed;
+								continue;
+							}
+						} else {
+							sub.flags &= ~SubscriberFlags.ToCheckDirty;
+						}
+					} else {
+						if (dirty) {
+							if (await sub.update()) {
+								sub = subSubs.sub as IComputed;
+								continue;
+							}
+						} else {
+							sub.flags &= ~SubscriberFlags.ToCheckDirty;
+						}
+						prevLink = subSubs;
+					}
+					link = prevLink.nextDep!;
+					if (link !== undefined) {
+						continue top;
+					}
+					sub = prevLink.sub as IComputed;
+					dirty = false;
+				} while (stack);
+			}
+			return dirty;
+		}
+		link = nextDep;
+	} while (true);
+}
