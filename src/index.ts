0f45dc34c310e202f6e54b20b539209001fadcca
Johnson Chu <johnsoncodehk@gmail.com>
2024-12-26 04:15:11 +0800

    refactor: avoid using `export * as` for tree-shaking friendly
    close #27

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,4 @@ export * from './effectScope.js';
 export * from './signal.js';
 export * from './system.js';
 export * from './types.js';
-export * as unstable from './unstable/index.js';
-/**
- * @deprecated Use `unstable` instead.
- */
-export * as Unstable from './unstable/index.js';
+export * from './unstable/index.js';
