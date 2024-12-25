56d181cc6778434a1a22336df2ba3ed541341b40
Puru Vijay <awesomepuruvj@gmail.com>
2024-12-26 00:27:40 +0530

    Push

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,14 @@ export * from './effectScope.js';
 export * from './signal.js';
 export * from './system.js';
 export * from './types.js';
-export * as unstable from './unstable/index.js';
-/**
- * @deprecated Use `unstable` instead.
- */
-export * as Unstable from './unstable/index.js';
+export {
+	asyncCheckDirty as unstable_asyncCheckDirty,
+	asyncComputed as unstable_asyncComputed,
+	AsyncComputed as unstable_AsyncComputed,
+	asyncEffect as unstable_asyncEffect,
+	AsyncEffect as unstable_AsyncEffect,
+	computedArray as unstable_computedArray,
+	computedSet as unstable_computedSet,
+	EqualityComputed as unstable_EqualityComputed,
+	equalityComputed as unstable_equalityComputed,
+} from './unstable/index.js';
