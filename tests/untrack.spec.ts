777b6d3da048d656c07aaf68385de059bbe03b1c
flamrdevs <flamrdevs@gmail.com>
2025-05-05 08:16:16 +0700

    test: update untrack tests

--- tests/untrack.spec.ts
+++ tests/untrack.spec.ts
@@ -1,16 +1,76 @@
 import { expect, test } from 'vitest';
-import { computed, pauseTracking, resumeTracking, signal } from '../src';
+import { computed, effect, effectScope, pauseTracking, resumeTracking, signal } from '../src';
 
-test('should pause tracking', () => {
+test('should pause tracking in computed', () => {
 	const src = signal(0);
+
+	let computedTriggerTimes = 0;
 	const c = computed(() => {
+		computedTriggerTimes++;
 		pauseTracking();
 		const value = src();
 		resumeTracking();
 		return value;
 	});
-	expect(c()).toBe(0);
 
-	src(1);
 	expect(c()).toBe(0);
+	expect(computedTriggerTimes).toBe(1);
+
+	src(1), src(2), src(3);
+	expect(c()).toBe(0);
+	expect(computedTriggerTimes).toBe(1);
+});
+
+test('should pause tracking in effect', () => {
+	const src = signal(0);
+	const is = signal(0);
+
+	let effectTriggerTimes = 0;
+	effect(() => {
+		effectTriggerTimes++;
+		if (is()) {
+			pauseTracking();
+			src();
+			resumeTracking();
+		}
+	});
+
+	expect(effectTriggerTimes).toBe(1);
+
+	is(1);
+	expect(effectTriggerTimes).toBe(2);
+
+	src(1), src(2), src(3);
+	expect(effectTriggerTimes).toBe(2);
+
+	is(2);
+	expect(effectTriggerTimes).toBe(3);
+
+	src(4), src(5), src(6);
+	expect(effectTriggerTimes).toBe(3);
+
+	is(0);
+	expect(effectTriggerTimes).toBe(4);
+
+	src(7), src(8), src(9);
+	expect(effectTriggerTimes).toBe(4);
+});
+
+test('should pause tracking in effect scope', () => {
+	const src = signal(0);
+
+	let effectTriggerTimes = 0;
+	effectScope(() => {
+		effect(() => {
+			effectTriggerTimes++;
+			pauseTracking();
+			src();
+			resumeTracking();
+		});
+	});
+
+	expect(effectTriggerTimes).toBe(1);
+
+	src(1), src(2), src(3);
+	expect(effectTriggerTimes).toBe(1);
 });
