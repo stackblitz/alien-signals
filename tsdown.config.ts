960c075e613b56e4a7f05c1821ced30910d127d5
flamrdevs <flamrdevs@gmail.com>
2025-05-10 02:13:29 +0700

    refactor: specify target in build config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-	format: ["esm", "cjs"],
-	outDir: "dist",
 	entry: ["src/index.ts", "src/system.ts"],
+	outDir: "dist",
+	format: ["esm", "cjs"],
+	target: "es2021",
 	platform: "neutral",
 	clean: true,
 	dts: true,
@@ -11,11 +12,8 @@ export default defineConfig({
 	minify: false,
 	outputOptions: (_, format) => {
 		const es = format === "es";
-
 		const dir = es ? "esm" : "cjs";
-
 		const ext = es ? "mjs" : "cjs";
-
 		return {
 			entryFileNames: `${dir}/[name].${ext}`,
 			chunkFileNames: `${dir}/[name]-[hash].${ext}`,
