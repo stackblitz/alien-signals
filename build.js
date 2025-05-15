6592fa8bda4c0460823f6d16e17dff7b1bc63c27
Johnson Chu <johnsoncodehk@gmail.com>
2025-05-15 18:52:45 +0800

    build: using TypeScript API to build dual package

---
+++ build.js
@@ -0,0 +1,57 @@
+// @ts-check
+const path = require('path');
+const ts = require('typescript');
+const config = ts.getParsedCommandLineOfConfigFile(
+	path.join(__dirname, 'tsconfig.json'),
+	undefined,
+	{
+		...ts.sys,
+		onUnRecoverableConfigFileDiagnostic: () => { },
+	}
+);
+
+if (config === undefined) {
+	console.error('Failed to parse tsconfig.json');
+	process.exit(1);
+}
+
+const typesProgram = ts.createProgram({
+	rootNames: config.fileNames,
+	options: config.options,
+	configFileParsingDiagnostics: config.errors,
+});
+const cjsProgram = ts.createProgram({
+	rootNames: config.fileNames,
+	options: {
+		...config.options,
+		declaration: false,
+		emitDeclarationOnly: false,
+		removeComments: true,
+		outDir: 'cjs',
+	},
+	configFileParsingDiagnostics: config.errors,
+});
+const esmProgram = ts.createProgram({
+	rootNames: config.fileNames,
+	options: {
+		...config.options,
+		declaration: false,
+		emitDeclarationOnly: false,
+		removeComments: true,
+		outDir: 'esm',
+		module: ts.ModuleKind.ESNext,
+	},
+	configFileParsingDiagnostics: config.errors,
+});
+
+typesProgram.emit(undefined, ts.sys.writeFile);
+cjsProgram.emit(undefined, (fileName, text) => {
+	fileName = fileName.slice(0, -'.js'.length) + '.cjs';
+	text = text.replace(/\.\/system\.js/g, './system.cjs');
+	ts.sys.writeFile(fileName, text);
+});
+esmProgram.emit(undefined, (fileName, text) => {
+	fileName = fileName.slice(0, -'.js'.length) + '.mjs';
+	text = text.replace(/\.\/system\.js/g, './system.mjs');
+	ts.sys.writeFile(fileName, text);
+});
