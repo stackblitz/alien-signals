9e4c14349eeb25c7730c5d616ed57ce376e4f674
Johnson Chu <johnsoncodehk@gmail.com>
2025-10-16 06:36:12 +0800

    feat: add size script for output size measurement

---
+++ size.js
@@ -0,0 +1,20 @@
+require('./build.js');
+
+const rolldown = require('rolldown');
+
+rolldown.build({
+	input: 'esm/index.mjs', output: { minify: true }, write: false
+}).then(built => {
+	console.log(`esm/index.mjs: ${(built.output[0].code.length / 1024).toFixed(2)} KB`);
+});
+rolldown.build({
+	input: 'esm/system.mjs', output: { minify: true }, write: false
+}).then(built => {
+	console.log(`esm/system.mjs: ${(built.output[0].code.length / 1024).toFixed(2)} KB`);
+});
+rolldown.build({ input: 'cjs/index.cjs', output: { minify: true }, write: false }).then(built => {
+	console.log(`cjs/index.cjs: ${(built.output[0].code.length / 1024).toFixed(2)} KB`);
+});
+rolldown.build({ input: 'cjs/system.cjs', output: { minify: true }, write: false }).then(built => {
+	console.log(`cjs/system.cjs: ${(built.output[0].code.length / 1024).toFixed(2)} KB`);
+});
